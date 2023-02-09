c3281d5b8d772ebe03447866111ef02bfb794e35
Nishal <nishaldevadiga2003@gmail.com>
2023-02-09 20:08:43 +0530

    2nd commit

--- app/src/main/java/com/example/basicactivityswitching02/Adapter/SecondRecyclerAdapter.kt
+++ app/src/main/java/com/example/basicactivityswitching02/Adapter/SecondRecyclerAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.basicactivityswitching02.Adapter
 
-class SecondRecyclerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.example.basicactivityswitching02.Model.Second
+import com.example.basicactivityswitching02.R
+
+class SecondRecyclerAdapter(val context: Context, val second: List<Second>): RecyclerView.Adapter<SecondRecyclerAdapter.Holder>() {
+    inner class Holder(itemView: View):RecyclerView.ViewHolder(itemView)
+    {
+        val secondTitle = itemView?.findViewById<TextView>(R.id.secondTitle)
+
+        fun bindSecond(second: Second,context: Context)
+        {
+            secondTitle?.text = second.title
+
+
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.second_list_item,parent,false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindSecond(second[position],context)
+    }
+
+    override fun getItemCount(): Int {
+        return second.count()
+    }
 }
--- app/src/main/java/com/example/basicactivityswitching02/Controller/SecondActivity.kt
+++ app/src/main/java/com/example/basicactivityswitching02/Controller/SecondActivity.kt
@@ -1,15 +1,26 @@
 package com.example.basicactivityswitching02.Controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.basicactivityswitching02.Adapter.SecondRecyclerAdapter
 import com.example.basicactivityswitching02.R
+import com.example.basicactivityswitching02.Service.Data
 import com.example.basicactivityswitching02.Utilities.EXTRA_FIRST
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var adapter: SecondRecyclerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         val second = intent.getStringExtra(EXTRA_FIRST)
+        adapter = SecondRecyclerAdapter(this, Data.second)
+
+        val layoutManager = LinearLayoutManager(this)
+        secondRecyclerView.layoutManager = layoutManager
+        secondRecyclerView.adapter = adapter
     }
 }
